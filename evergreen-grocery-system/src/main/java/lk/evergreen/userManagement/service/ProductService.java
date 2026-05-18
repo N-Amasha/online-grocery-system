ae4bb4330ff70219667e2c09ac19054963e27867
Sewmahal D.H.P <hirusha.sewmahal.lk.dev@gmail.com>
2026-05-18 22:45:21 +0530

    Initial migration from Core Java project to Spring Boot framework

--- evergreen-grocery-system/src/main/java/lk/evergreen/userManagement/service/ProductService.java
+++ evergreen-grocery-system/src/main/java/lk/evergreen/userManagement/service/ProductService.java
@@ -2,6 +2,7 @@ package lk.evergreen.grocery.service;
 
 import lk.evergreen.grocery.entity.Product;
 import lk.evergreen.grocery.repository.ProductRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,6 @@ public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
-
 
     public List<Product> listAll() {
         return productRepository.findAllWithCategoryOrdered();
@@ -27,5 +27,18 @@ public class ProductService {
         return productRepository.searchByName(query);
     }
 
-}
+    public Product create(Product product) {
+        return productRepository.save(product);
+    }
 
+    public Product update(Long id, Product product) {
+
+        product.setId(id);
+
+        return productRepository.save(product);
+    }
+
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
+}
