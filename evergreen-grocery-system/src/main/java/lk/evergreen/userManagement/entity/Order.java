515dbe73fd2c30ac8c98b05dc032e1a4ad4a3f9e
it25102331 <it25102331@my.sliit.lk>
2026-05-18 14:37:28 +0530

    Initial migration from Core Java project to Spring Boot framework

---
+++ evergreen-grocery-system/src/main/java/lk/evergreen/userManagement/entity/Order.java
@@ -0,0 +1,12 @@
+package lk.evergreen.grocery.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
